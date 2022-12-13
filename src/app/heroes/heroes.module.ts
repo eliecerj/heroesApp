86981cabeb44bcf483f73df1bedbbe7c795dd300
Eliecer Arteaga <eliecer.arteaga@externos.sura.cl>
2022-12-13 18:17:59 -0300

    implementing material

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 
@@ -8,6 +9,7 @@ import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -21,7 +23,9 @@ import { ListComponent } from './pages/list/list.component';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    FlexLayoutModule,
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
